fcef9179c8e8980834e907821633e44999da5421
RoshanNeelmegam <98408299+RoshanNeelmegam@users.noreply.github.com>
2023-10-24 18:40:01 +0530

    Update DeleteNode.c

--- LinkedList/DeleteNode.c
+++ LinkedList/DeleteNode.c
@@ -1,83 +1,78 @@
-#include<stdio.h>
-#include<stdlib.h>
+// Online C compiler to run C program online
+#include <stdio.h>
+#include <stdlib.h>
 
-struct node
-{   
+struct node{
     int data;
     struct node *next;
 };
 
-void deleteNode(struct node **head, int key)
-{     
-     //Write your code here
-     struct node *temp;
-     if((*head)->data == key){
-         temp = *head;
-         *head = (*head)->next;
-         free(temp);
-     }
-     else{
-         struct node *current;
-         temp = *head;
-         while (temp->next->data != key){
-             temp = temp->next;
-         }
-         current = temp;
-         temp = temp->next;
-         current->next = current->next->next;
-         free(temp);
-     }
-
-}
-
-//Don't change the below code
-void addLast(struct node **head, int val)
-{   
-    struct node *newNode = malloc(sizeof(struct node));
-    newNode->data = val;
-    newNode->next = NULL;
-    
-    if(*head == NULL)
-         *head = newNode;
-    else
-    {   
-        struct node *lastNode = *head;
-
-        while(lastNode->next != NULL)
-        {
-            lastNode = lastNode->next;
+void addLast(struct node **headptr, int val){
+    struct node *newnode = malloc(sizeof(struct node));
+    newnode->data = val;
+    if (*headptr == NULL){
+        *headptr = newnode;
+        (*headptr)->next=NULL;
+    }  
+    else{
+        struct node *temp = *headptr;
+        while(temp->next != NULL){
+            temp = temp->next;
         }
-
-        lastNode->next = newNode;
-    }
-
-}
-
-void printList(struct node *head)
-{
-    struct node *temp = head;
-
-    while(temp != NULL)
-    {
-         printf("%d ", temp->data);
-         temp = temp->next;
+        temp->next = newnode;
     }
 }
 
-int main()
-{
+void printList(struct node **headptr){
+    struct node *temp = *headptr;
+    while(temp!=NULL){
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+}
+
+void delNode(struct node **headptr, int val){
+    struct node *temp = *headptr;
+    if (temp->data == val){
+        *headptr = temp->next;
+        free(temp);
+    }
+    else{
+        while (temp->next->data != val){
+            temp = temp->next;
+            if (temp->next== NULL){
+                temp = temp->next;
+                break;
+            }
+        }
+        if (temp!=NULL){
+            struct node *current = temp;
+            temp = temp->next;
+            if (temp!=NULL){
+                current->next = temp->next;
+            }
+            else{
+                current->next = NULL;
+            }
+        }
+        else{
+            printf("Node not found");
+        }
+    }
+}
+
+
+int main() {
+    // Write C code here
     struct node *head = NULL;
-
-    addLast(&head,10);
-    addLast(&head,20);
-    addLast(&head,30);
-
-    int key;
-    scanf("%d",&key);
-
-    deleteNode(&head, key);
-
-    printList(head);
+    addLast(&head, 10);
+    addLast(&head, 20);
+    addLast(&head, 30);
+    printList(&head);
+    printf("\n");
+    delNode(&head, 60);
+    printf("\n");
+    printList(&head);
 
     return 0;
 }
